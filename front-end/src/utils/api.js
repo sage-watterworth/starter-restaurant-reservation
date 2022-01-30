cdec75497ad58e95e637b1977b45276f5b744ea0
Sage Watterworth <watter.sage@gmail.com>
2022-01-30 13:42:03 -0500

    update api url sans /

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -5,7 +5,7 @@
 import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
-const API_BASE_URL = "https://backend-reservation.herokuapp.com/"
+const API_BASE_URL = "https://backend-reservation.herokuapp.com"
   // process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
 
 
